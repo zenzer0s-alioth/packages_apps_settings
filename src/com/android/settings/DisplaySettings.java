97478b36ef3c7f19bd5e2a34d69d85f33fff1884
zenzer0s <praveenzalaki200@gmail.com>
2026-01-26 02:40:53 +0000

    ..

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -37,7 +37,7 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.search.SearchIndexable;
-
+import com.android.settings.display.DataDisabledIconPreferenceController;
 import java.util.ArrayList;
 import java.util.List;
 
